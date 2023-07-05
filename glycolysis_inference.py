4706d80a92d5438ace30ccfddcb5c27be0afa5da
janisshin <janiseshin@gmail.com>
2023-07-04 17:58:19 -0700

    code A run

--- glycolysis_inference.py
+++ glycolysis_inference.py
@@ -1,4 +1,8 @@
-path = 'MODEL1303260011_cobra.ant'
+# handy-dandy
+import os
+import sys
+from tqdm import tqdm
+
 dataPath='MODEL1303260011_pt10.csv'
 
 FOLDER_NAME = 'glycolysis/'
@@ -7,11 +11,10 @@ DATA_OMISSION_CODE = 'A'
 ADVI_ITERATIONS = 100000 # 60000
 FAIL_LOG_FILE = f'failed-{DATA_OMISSION_CODE}.log'
 DATA_FOLDER_NAME = 'generated_data/'
+name_of_script = sys.argv[1]
+LEARNING_RATE = 1E-2
 
-# handy-dandy
-import os
-import sys
-from tqdm import tqdm
+
 
 # arrays/dataframes
 import numpy as np
@@ -47,14 +50,13 @@ import cloudpickle
 import emll
 from emll.util import initialize_elasticity
 
-######################################################################
-
 itr = ADVI_ITERATIONS
 folder_name = FOLDER_NAME
 
-r = te.loada(path)
+r = te.loada("MODEL1303260011.ant")
+s = te.loada("MODEL1303260011_cobra.ant")
 with open("temp.txt", "w") as f:
-    f.write(r.getSBML())
+    f.write(s.getSBML())
 
 model = cobra.io.read_sbml_model("temp.txt")
 os.remove("temp.txt") 
@@ -81,22 +83,29 @@ v = data[v_cols]
 # desired product here, we arbitrarily choose a random species as 
 # our desired product
 # target_rxn_i = -1
-desired_product = 'EtOH' # y_cols[target_rxn_i] # the boundary form of the desired product
+desired_product = 'succinate_branch' # y_cols[target_rxn_i] # the boundary form of the desired product
 # the strain index at which the level of desired product is highest
-ref_ind = data.idxmax()[desired_product] 
+ref_ind = data.idxmax()['EX_' + desired_product] 
 # list of boundary reactions
 exRxns = [i.id for i in model.reactions if 'EX' in i.id] 
 # boundary reaction that produces desired product
-target_rxn = [i for i in exRxns if desired_product[1:] == i[4:]][0]
+target_rxn = [i for i in exRxns if desired_product == i[3:]][0]
 desired_product = target_rxn[3:] # the internal form of the desired product
-    
+
 e_star = e.iloc[ref_ind].values
 x_star = x.iloc[ref_ind].values
 y_star = y.iloc[ref_ind].values
 v_star = v.iloc[ref_ind].values
 
+e_star[e_star == 0] = 1e-6
 v_star[v_star == 0] = 1e-9
 y_star[y_star == 0] = 1e-6
+y[y <= 0] = 1e-6
+
+assert (len(e.values[e.values <= 0]) == 0)
+assert (len(y.values[y.values <= 0]) == 0)
+assert (len(e_star[e_star <= 0]) == 0)
+assert (len(y_star[y_star <= 0]) == 0)
 
 # Normalize to reference values (and drop trivial measurement)
 en = e.divide(e_star)
@@ -104,13 +113,22 @@ xn = x.divide(x_star)
 yn = y.divide(y_star)
 vn = v.divide(v_star)
 
+# get rid of any 0 values
+vn[vn <= 0] = 1e-6 # need to drop Nan values
+en[en <= 0] = 1e-6
+yn[yn <= 0] = 1e-6
+
 en = en.drop(ref_ind)
 xn = xn.drop(ref_ind)
 yn = yn.drop(ref_ind)
 vn = vn.drop(ref_ind)      
 
+# Correct negative flux values at the reference state
 N[:, v_star < 0] = -1 * N[:, v_star < 0]
 v_star = np.abs(v_star)
+
+# Correct all 0 fluxes. Cannot divide nor take log of 0
+v_star[v_star == 0] = 1e-9
 
 Ex = emll.create_elasticity_matrix(model)
 Ey = np.zeros((nr, len(y_cols))) # (reactions, number of external species)
@@ -132,18 +150,15 @@ with pm.Model() as pymc_model:
 
 with pymc_model:
     # Error priors. 
-    #v_err = pm.HalfNormal('v_error', sigma=0.05, initval=.1)
-    #x_err = pm.HalfNormal('x_error', sigma=0.05, initval=.1)
     y_err = pm.HalfNormal('y_error', sigma=0.05, initval=.01)
-    #e_err = pm.HalfNormal('e_error', sigma=10, initval=.01)
-   
+
     # Calculate steady-state concentrations and fluxes from elasticities
-    chi_ss, vn_ss_x = ll.steady_state_aesara(Ex_t, Ey_t, en, yn)
-    y_ss, vn_ss_y = ll.steady_state_aesara(Ey_t, Ex_t, en, xn)
+    chi_ss, vn_ss_x = ll.steady_state_aesara(Ex_t, Ey_t, en.to_numpy(), yn.to_numpy())
+    y_ss, vn_ss_y = ll.steady_state_aesara(Ey_t, Ex_t, en.to_numpy(), xn.to_numpy())
 
     # Error distributions for observed steady-state concentrations and fluxes
     
-    v_hat_obs = pm.Normal('v_hat_obs', mu=vn_ss_x, sigma=0.1, observed=vn) # both bn and v_hat_ss are (28,6)
+    v_hat_obs = pm.Normal('v_hat_obs', mu=vn_ss_y, sigma=0.1, observed=vn) # both bn and v_hat_ss are (28,6)
     chi_obs = pm.Normal('chi_obs', mu=chi_ss, sigma=0.1, observed=xn) # chi_ss and xn is (28,4)
     y_obs = pm.Normal('y_obs', mu=y_ss, sigma=y_err, observed=yn)
     # e_obs = pm.Normal('e_obs', mu=1, sigma=0.1, observed=en)
@@ -154,7 +169,7 @@ with pymc_model:
 # sampling
 with pymc_model:
     approx = pm.ADVI()
-    hist = approx.fit(n=itr, obj_optimizer=pm.adagrad_window(learning_rate=5E-3), obj_n_mc=1)
+    hist = approx.fit(n=itr, obj_optimizer=pm.adagrad_window(learning_rate=LEARNING_RATE), obj_n_mc=1)
 
 with pymc_model:
     trace = hist.sample(1000)    
@@ -172,15 +187,14 @@ ey_labels = np.array([['$\epsilon_{' + '{0},{1}'.format(rlabel, mlabel) + '}$'
 
 e_labels = np.hstack((ex_labels, ey_labels))
 
-# generating and storing results
-dataset_name = dataPath.split(".")[0]
 
 cloudpickle.dump({'advi': approx,
 'approx': approx,
 'trace': trace,
 'trace_prior': trace_prior,
+'ppc': ppc_vi,
 'e_labels': e_labels,
 'r_labels': r_labels,
 'm_labels': m_labels,
-'ll': ll}, file=open(f'folder_name + {dataset_name}_advi.pgz', "wb"))
-
+'y_labels': y_labels,
+'ll': ll}, file=open(f'folder_name + {name_of_script}_advi.pgz', "wb"))
